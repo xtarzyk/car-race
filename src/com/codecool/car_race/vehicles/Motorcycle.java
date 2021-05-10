fb8163f19e098712a7fc065e7339b5d5a0b9cbda
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2021-05-10 22:26:36 +0200

    abstract class Vehicle is done, implemented prepareForLap method to subclasses

--- src/com/codecool/car_race/vehicles/Motorcycle.java
+++ src/com/codecool/car_race/vehicles/Motorcycle.java
@@ -1,4 +1,10 @@
 package com.codecool.car_race.vehicles;
 
+import com.codecool.car_race.Race;
+
 public class Motorcycle extends Vehicle{
+    @Override
+    public void prepareForLap(Race race) {
+
+    }
 }
--- src/com/codecool/car_race/vehicles/Vehicle.java
+++ src/com/codecool/car_race/vehicles/Vehicle.java
@@ -1,4 +1,21 @@
 package com.codecool.car_race.vehicles;
 
+import com.codecool.car_race.Race;
+
 public abstract class Vehicle {
+    protected final String name;
+    protected final int normalSpeed;
+    protected int actualSpeed;
+    protected int distanceTravelled;
+
+    protected Vehicle() {
+        this.name = "Vehicle";
+        this.normalSpeed = 100;
+    }
+
+    public abstract void  prepareForLap(Race race);
+
+    public final void moveForAnHour() {
+        distanceTravelled += actualSpeed;
+    }
 }
